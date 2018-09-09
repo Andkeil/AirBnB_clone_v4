ac22d89fda3774d9c583a9f3442a095a069b59fe
Alex Allen <sanjurosavaes@outlook.com>
2018-09-09 21:13:56 +0000

    getting container address on port 80; css updates

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,7 +1,7 @@
 $(document).ready(function () {
-  $.get("http://0.0.0.0:5001/api/v1/status/", function (data) {
-    console.log(data);
-    if (status === 'OK') {
+  $.get("http://172.31.54.208:40228/api/v1/status/", function (data) {
+    console.log(data.status);
+    if (data.status === 'OK') {
     $('DIV#api_status').addClass('available')
   } else {
     $('DIV#api_status').removeClass('available')
