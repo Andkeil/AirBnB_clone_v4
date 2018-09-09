5b991534a473fed3568e7f486b71180f5e738770
Alex Allen <325@holbertonschool.com>
2018-09-09 17:31:26 +0000

    task two buggy script

--- web_dynamic/static/scripts/1-hbnb.js
+++ web_dynamic/static/scripts/1-hbnb.js
@@ -1,14 +1,36 @@
-$( document ).ready(function() {
-	if (typeof id_list == "undefined" || !(id_list instanceof Array)) {
-	    var id_list = [];
-   	    console.log(id_list);
-	}
+$(document).ready(function () {
+  function rmID (arr) {
+    let what;
+    let a = arguments;
+    let L = a.length;
+    let ax;
+    while (L > 1 && arr.length) {
+      what = a[--L];
+      while ((ax = arr.indexOf(what)) !== -1) {
+        arr.splice(ax, 1);
+      }
+    }
+    return arr;
+  }
 
-    $('input[type=checkbox]').change(function() {
-	if (this.checked) {
-	    id_list.push(this.getAttribute("data-id"));
-	console.log(id_list);
-}
-})
-
+  let idList = [];
+  $('input[type=checkbox]').change(function () {
+    if (this.checked) {
+      idList.push(this.getAttribute('data-id'));
+      console.log(idList);
+/*    	    for (let i = 0; i < id_list.length; i++) {
+		if (i = 0) {
+		    $("div", "h4").text(id_list[i]);
+		} else {
+		    $("div", "h4").append(id_list[i]);
+		    }
+	 }
+*/
+    }
+    if (!this.checked) {
+      rmID(idList, this.getAttribute('data-id'));
+      console.log(idList);
+    }
+  });
+  $('div', 'h4').text('goodluck');
 });
