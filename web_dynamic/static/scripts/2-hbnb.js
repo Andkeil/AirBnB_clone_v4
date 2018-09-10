bc536f42c9f6c8eb43dab43dcc44a707ce46def0
Andrew Keil <monkeyninja147@gmail.com>
2018-09-10 17:27:00 +0000

    Semistandard fixes #2

--- web_dynamic/static/scripts/2-hbnb.js
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -1,34 +1,33 @@
 $(document).ready(function () {
-  $.get("http://0.0.0.0:5001/api/v1/status/", function (data) {
+  $.get('http://0.0.0.0:5001/api/v1/status/', function (data) {
     console.log(data.status);
     if (data.status === 'OK') {
-    $('DIV#api_status').addClass('available')
-  } else {
-    $('DIV#api_status').removeClass('available')
-  };
-});
+      $('DIV#api_status').addClass('available');
+    } else {
+      $('DIV#api_status').removeClass('available');
+    }
+  });
   let amenityDict = {};
   $('input[type=checkbox]').change(function () {
     if (this.checked) {
       amenityDict[this.getAttribute('data-id')] = this.getAttribute('data-name');
-    }
-    else if (!this.checked) {
+    } else if (!this.checked) {
       delete amenityDict[this.getAttribute('data-id')];
     }
-      let i = 0;
-      let string = "";
-      Object.keys(amenityDict).forEach(function (key) {
-	if (i === 0) {
-	  string += amenityDict[key];
-	} else {
-	  string += ", ";
-	  string += amenityDict[key];
-	}
-	i++;
-        $("div.amenities h4").text(string);
-      });
-      if (!Object.keys(amenityDict).length) {
-        $("div.amenities h4").empty();
-      };
+    let i = 0;
+    let string = '';
+    Object.keys(amenityDict).forEach(function (key) {
+      if (i === 0) {
+        string += amenityDict[key];
+      } else {
+        string += ', ';
+        string += amenityDict[key];
+      }
+      i++;
+      $('div.amenities h4').text(string);
+    });
+    if (!Object.keys(amenityDict).length) {
+      $('div.amenities h4').empty();
+    }
   });
 });
